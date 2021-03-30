722ec78621adf5c60ec7b270536b846677d7b54f
alesamv <alesamv@gmail.com>
2021-03-30 14:40:18 -0600

    Finishing

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-$(document).ready(function(){
+//  $(document).ready(function(){
 
-    $('.popup').delay(4000).fadeIn(500);
+//      $('.popup').delay(4000).fadeIn(500);
 
-    $('.popup .cerrar').click(function(){
-        $('.popup').fadeOut();
-    })
+//      $('.popup .cerrar').click(function(){
+//          $('.popup').fadeOut();
+//      })
+// })
+
+$('.menu-icon').on('click', function(){
+    $('nav').slideToggle();
 })
